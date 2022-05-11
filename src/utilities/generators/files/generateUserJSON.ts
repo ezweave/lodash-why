aaf2f62ee77f85f2478519dd57c3a795a517474c
Matt Weaver <ezweave@gmail.com>
2022-05-11 09:01:10 -0600

    moving to latest es

--- src/utilities/generators/files/generateUserJSON.ts
+++ src/utilities/generators/files/generateUserJSON.ts
@@ -1,3 +1,11 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 console.log('Generating user data...');
+
+const maleNameResponse = await axios.get(
+  'https://www.randomlists.com/data/names-male.json'
+);
+
+console.warn(maleNameResponse);
+
+export {};
